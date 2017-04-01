8da4abe5220ce65344fde29cc197557dba477d90
Anson Zeall <anson.zeall@gmail.com>
2017-04-01 15:38:32 +0000

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,8 @@
 class VisitorsController < ApplicationController
 
   def new
+    
     @owner = Owner.new
-    render 'visitors/new'
   end
 
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -8,6 +8,8 @@ class Owner
     end
     
     def countdown
+        Rails.logger.debug 'DEBUG: entering Owner countdown method'
+        Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
         today = Date.today
         birthday = Date.new(today.year, birthdate.month, birthdate.day)
         if birthday > today
